ed7f9b7362c373df4705bb99d22c190303b1ee5e
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-04-04 14:40:21 +0100

    print state diff as json

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -1265,6 +1265,12 @@ func (s *StateDB) commitAndFlush(block uint64, deleteEmptyObjects bool, noStorag
 	if err != nil {
 		return nil, err
 	}
+	jsonBytes, err := json.Marshal(ret)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(string(jsonBytes))
+
 	// Commit dirty contract code if any exists
 	if db := s.db.TrieDB().Disk(); db != nil && len(ret.codes) > 0 {
 		batch := db.NewBatch()
